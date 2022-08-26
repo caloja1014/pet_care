1fbdda95efad0f8aa69293531bee45c8e9e1ba4d
caloja1014 <caloja@espol.edu.ec>
2022-08-25 20:58:41 -0500

    Setting get structure

---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Obx(
+          () => Text(
+            '${controller.count.value}',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.increment,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
