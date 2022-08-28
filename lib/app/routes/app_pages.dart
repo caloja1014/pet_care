a42b0717f9ffea9e7de8dc54743d533907f612e2
caloja1014 <caloja@espol.edu.ec>
2022-08-27 20:48:51 -0500

    register local_vet

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -6,13 +6,15 @@ import '../modules/login/bindings/login.binding.dart';
 import '../modules/login/views/login.view.dart';
 import '../modules/register/bindings/register.binding.dart';
 import '../modules/register/views/register.view.dart';
+import '../modules/register_local_vet/bindings/register_local_vet.binding.dart';
+import '../modules/register_local_vet/views/register_local_vet.view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.LOGIN;
+  static const INITIAL = Routes.REGISTER_LOCAL_VET;
 
   static final routes = [
     GetPage(
@@ -27,8 +29,13 @@ class AppPages {
     ),
     GetPage(
       name: _Paths.LOGIN,
-      page: () =>  LoginView(),
+      page: () => LoginView(),
       binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.REGISTER_LOCAL_VET,
+      page: () =>  RegisterLocalVetView(),
+      binding: RegisterLocalVetBinding(),
     ),
   ];
 }
