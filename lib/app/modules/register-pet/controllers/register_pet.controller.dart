b5a42db0b0957ec8a9c49e340082fe1520d09320
Noelia Alejandra Intriago Sánchez <noinsanc@espol.edu.ec>
2022-09-05 00:16:47 -0500

    Post for pets (incomplete)

--- lib/app/modules/register-pet/controllers/register_pet.controller.dart
+++ lib/app/modules/register-pet/controllers/register_pet.controller.dart
@@ -1,6 +1,14 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:pet_care/app/data/serivces/connection_wrapper.dart';
+import 'package:http/http.dart' as http;
 
 class RegisterPetController extends GetxController {
+  final ConnectionWrapper connectionWrapper;
+  RegisterPetController({
+    required this.connectionWrapper,
+  });
   //TODO: Implement RegisterPetController
 
   final count = 0.obs;
@@ -19,5 +27,36 @@ class RegisterPetController extends GetxController {
     super.onClose();
   }
 
+  Future<http.Response> postPet({
+    required String breed,
+    required String name,
+    required int age,
+    required String petownerIdentification,
+  }) {
+    Map<String, dynamic> body = {
+      'breed': breed,
+      'name': name,
+      'age': age,
+      'petownerIdentification': petownerIdentification,
+    };
+    final String encodedData = json.encode(body);
+    http.Client client = new http.Client();
+    return client.post(
+      Uri.parse(
+        ConnectionWrapper.ROOT_URL + '/pet',
+      ),
+      body: encodedData,
+      headers: {"Content-Type": "application/json"},
+    );
+  }
+
+  int calculateAge(birthDate){
+    double ages = 0;
+    DateTime actual = DateTime.now();
+
+    ages = actual.difference(birthDate).inDays/360;
+
+    return ages.round();
+  }
   void increment() => count.value++;
 }
