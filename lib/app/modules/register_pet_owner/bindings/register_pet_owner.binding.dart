0dd7c3c8c71659c8056547848ead70b72dcc3870
Noelia Alejandra Intriago Sánchez <noinsanc@espol.edu.ec>
2022-09-04 22:39:50 -0500

    Create post for petowners

--- lib/app/modules/register_pet_owner/bindings/register_pet_owner.binding.dart
+++ lib/app/modules/register_pet_owner/bindings/register_pet_owner.binding.dart
@@ -1,12 +1,14 @@
 import 'package:get/get.dart';
+import 'package:pet_care/app/data/serivces/connection_wrapper.dart';
 
 import '../controllers/register_pet_owner.controller.dart';
 
 class RegisterPetOwnerBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut(() => ConnectionWrapper());
     Get.lazyPut<RegisterPetOwnerController>(
-      () => RegisterPetOwnerController(),
+      () => RegisterPetOwnerController(connectionWrapper: Get.find()),
     );
   }
 }
